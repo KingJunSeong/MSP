d4764e88c167cc15e86791f2ee84bd946d115fbf
Claude Agnes <rlawnstjddl12@gmail.com>
2023-04-18 08:52:39 +0900

    delete apikey

--- MSP/Utils/APIKey.cs
+++ MSP/Utils/APIKey.cs
@@ -8,6 +8,6 @@ namespace MSP.Utils
 {
     public class APIKey
     {
-        public static string apikey = "7cf8332e2a8020ad532a7d474882e9b48291deadec7b13b3b5c9a14c7b91df6c";
+        public static string apikey = "api key here";
     }
 }
